da7a6fb9a88ba87708ec43a0a5dbebf546d95ad7
Finta Gábor <fintag1@kookaburra.org.aalto.fi>
2018-04-17 14:03:48 +0300

    cp2a 3pt

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -5,6 +5,8 @@
 #include <vector>
 #include <numeric>
 
+constexpr int nPad = 4;
+
 double MeanOfRow(const int& nx, const float* data, const int& y)
 {
     std::vector<double> means(4, 0.0);
@@ -15,32 +17,38 @@ double MeanOfRow(const int& nx, const float* data, const int& y)
     return (means[0] + means[1] + means[2] + means[3]) / (double)nx;
 }
 
-double CalculateDenominator(const int& nx, const int& y, const double* normalized)
+double CalculateDenominator(const int& nx, const int& y, const std::vector<double>& normalized, const int& nNewX)
 {
     std::vector<double> denominators(4, 0.0);
     
     for(int x = 0; x < nx; ++x)
     {
-	denominators[x % 4] += pow(normalized[x + y * nx], 2);
+	denominators[x % 4] += pow(normalized[x + y * nNewX], 2);
     }
     return sqrt(denominators[0] + denominators[1] + denominators[2] + denominators[3]);
 }
 
-double CalculateSum(const int& nx, const int& y, const int& x, const double* normalized)
+double CalculateSum(const int& nx, const int& y, const int& x, const std::vector<double>& normalized, const int& nNewX)
 {
     std::vector<double> sums(4, 0.0);
 
-    for(int k = 0; k < nx; ++k)
+    for(int k = 0; k < nx/*nx*/; k += 4)
     {
-	sums[k % 4] += normalized[k + y * nx] * normalized[k + x * nx];
+	for(int i = 0; i < 4; ++i)
+	{
+	    sums[i] += normalized[k + i + y * nNewX] * normalized[k + i + x * nNewX];
+	}
+	//sums[k % 4] += normalized[k + y * nx] * normalized[k + x * nx];
     }
     return std::accumulate(sums.begin(), sums.end(), 0.);
 }
 
 void correlate(int ny, int nx, const float* data, float*result)
 {
-    double* normalized = (double*)malloc(sizeof(double) * nx * ny);
-    //std::vector<double> normalized(nx * ny);
+    int nPadded = (nx + nPad - 1) / nPad;
+    int nNewX = nPadded * 4;
+    //double* normalized = (double*)malloc(sizeof(double) * nNewX * ny);
+    std::vector<double> normalized(nNewX * ny, 0.0);
 
     //first normalization
     
@@ -49,18 +57,18 @@ void correlate(int ny, int nx, const float* data, float*result)
 	double mean = MeanOfRow(nx, data, y);
 	for(int x = 0; x < nx; ++x)
 	{
-	    normalized[x + y * nx] = (double)(data[x + y * nx]) - mean;
+	    normalized[x + y * nNewX] = (double)(data[x + y * nx]) - mean;
 	}
     }
     //second normalization
 
     for(int y = 0; y < ny; ++y)
     {
-	double denominator = CalculateDenominator(nx, y, normalized);
+	double denominator = CalculateDenominator(nx, y, normalized, nNewX);
       
 	for(int x = 0; x < nx; ++x)
 	{
-	    normalized[x + y * nx] /= denominator;
+	    normalized[x + y * nNewX] /= denominator;
 	}
     }
 
@@ -71,9 +79,9 @@ void correlate(int ny, int nx, const float* data, float*result)
     {
         for(int x = y; x < ny; ++x)
 	{
-	    double sum = CalculateSum(nx, y, x, normalized);
+	    double sum = CalculateSum(nx, y, x, normalized, nNewX);
 	    result[x + y * ny] = (float)sum;
 	}
     }
-    free(normalized);
+    //free(normalized);
 }
