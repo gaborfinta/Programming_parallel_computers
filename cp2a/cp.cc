b1ad20d0fd3b3cafeff3146f7f2ff28c837046c3
Finta Gábor <fintag1@vuorikide.org.aalto.fi>
2018-04-19 18:56:38 +0300

    cp2a 3 pt, thanks for feedback, helped a lot

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -9,44 +9,63 @@ constexpr int nPad = 4;
 
 double MeanOfRow(const int& nx, const float* data, const int& y)
 {
-    std::vector<double> means(4, 0.0);
+    //std::vector<double> means(4, 0.0);
+    double* sums = (double*)malloc(sizeof(double) * nPad);
+    for(int i = 0; i < nPad; ++i)
+    {
+	sums[i] = 0.0;
+    }
     for(int i = 0; i < nx; ++i)
     {
-	means[i % 4] += (double)data[i + y * nx];
+	sums[i % nPad] += (double)data[i + y * nx];
     }
-    return (means[0] + means[1] + means[2] + means[3]) / (double)nx;
-}
+    double sum = std::accumulate(sums, sums + nPad, 0.);
+    free(sums);
+    return sum / (double)nx;
+} 
 
 double CalculateDenominator(const int& nx, const int& y, const std::vector<double>& normalized, const int& nNewX)
 {
-    std::vector<double> denominators(4, 0.0);
-    
+    double* denominators = (double*)malloc(sizeof(double) * nPad);
+    //std::vector<double> denominators(4, 0.0);
+    for(int i = 0; i < nPad; ++i)
+    {
+	denominators[i] = 0.0;
+    }
     for(int x = 0; x < nx; ++x)
     {
-	denominators[x % 4] += pow(normalized[x + y * nNewX], 2);
+	denominators[x % nPad] += pow(normalized[x + y * nNewX], 2);
     }
-    return sqrt(denominators[0] + denominators[1] + denominators[2] + denominators[3]);
+    double denominator = std::accumulate(denominators, denominators + nPad, 0.);
+    free(denominators);
+    return sqrt(denominator);
 }
 
 double CalculateSum(const int& nx, const int& y, const int& x, const std::vector<double>& normalized, const int& nNewX)
 {
-    std::vector<double> sums(4, 0.0);
-
-    for(int k = 0; k < nx/*nx*/; k += 4)
+    //std::vector<double> sums(4, 0.0);
+    double* sums = (double*)malloc(sizeof(double) * nPad);
+    for(int i = 0; i < nPad; ++i)
     {
-	for(int i = 0; i < 4; ++i)
+	sums[i] = 0.0;
+    }
+    for(int k = 0; k < nx/*nx*/; k += nPad)
+    {
+	for(int i = 0; i < nPad; ++i)
 	{
 	    sums[i] += normalized[k + i + y * nNewX] * normalized[k + i + x * nNewX];
 	}
 	//sums[k % 4] += normalized[k + y * nx] * normalized[k + x * nx];
     }
-    return std::accumulate(sums.begin(), sums.end(), 0.);
+    double sum = std::accumulate(sums, sums + nPad, 0.);
+    free(sums);
+    return sum;
 }
 
 void correlate(int ny, int nx, const float* data, float*result)
 {
     int nPadded = (nx + nPad - 1) / nPad;
-    int nNewX = nPadded * 4;
+    int nNewX = nPadded * nPad;
     //double* normalized = (double*)malloc(sizeof(double) * nNewX * ny);
     std::vector<double> normalized(nNewX * ny, 0.0);
 
